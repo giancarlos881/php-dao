164b76be23ca9ed08e8cc361df5651a8b87ed65b
Gian Carlos Dal Pont <giandalpont@gmail.com>
2018-05-31 03:01:58 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once("config.php");
+
+//$sql = new Sql();
+//$usuario = $sql -> select("SELECT* FROM tb_usuarios");
+//echo json_encode($usuario);
+
+
+// Carrega um usuarios
+//$user = new Usuario();
+//$user->loadbyId(4);
+//echo $user;
+
+
+// Carrega uma lista de Usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+// Carrega uma lista de usuarios buscando pelo login
+//$busca = Usuario::search("a");
+//echo json_encode($busca);
+
+
+// Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("gian","gian");
+//echo $usuario;
+
+
+// CRIANDO UM NOVO USUARIO
+//$aluno = new Usuario("gian", "naig");
+//$aluno->setDeslogin("123");
+//$aluno->setDessenha("321");
+//$aluno->insert();
+//echo $aluno;
+
+
+// UPDATE Alterar um usuario
+//$usuario = new Usuario();
+//$usuario->loadbyId(11);
+//$usuario->update("teste", "teste");
+//echo $usuario;
+
+
+//DELETE Deletar um usuário
+//$usuario = new Usuario();
+//$usuario->loadbyId(9);
+//$usuario->delete();
+//echo $usuario;
+
+?>
